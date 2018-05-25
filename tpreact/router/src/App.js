da70a4e9114133d792afcc60a4c74e9a7303ec31
Damien Marchand <damien.marchand1@gmail.com>
2018-05-25 10:15:02 +0200

    6.3 Router switch

---
+++ tpreact/router/src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Home} from './routes/Home';
+import {About} from './routes/About';
+import {Params} from './routes/Params';
+import {Hierarchical} from './routes/Hierarchical';
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/about" component={About}/>
+                        <Route exact path="/:custom" component={Params}/>
+                        <Route path="/hierarchical" component={Hierarchical}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+/*import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+);
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li>
+                <Link to={`${match.url}/rendering`}>
+                    Rendering with React
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>
+                    Components
+                </Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/props-v-state`}>
+                    Props v. State
+                </Link>
+            </li>
+        </ul>
+        <Route path={`${match.path}/:topicId`} component={Topic}/>
+        <Route exact path={match.path} render={() => (
+            <h3>Please select a topic.</h3>
+        )}/>
+    </div> );
+
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+        </div>
+    </Router> );
+
+export default BasicExample;*/
---
+++ tpreact/router/src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Home = (props) => {
+
+    console.log('Props : ', props);
+
+    return (
+        <div>
+            <h1>Home route</h1>
+        </div>
+    )
+};
